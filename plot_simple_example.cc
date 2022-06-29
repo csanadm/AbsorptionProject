54c251cbab35e1bc51aa2d3a5045b7c3435761e6
csanadm <38218165+csanadm@users.noreply.github.com>
2022-06-29 10:14:29 +0200

    Add files via upload

---
+++ plot_simple_example.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+#include "TF1.h"
+#include "TH1.h"
+#include "TCanvas.h"
+#include "TFitResult.h"
+#include "TLatex.h"
+#include "TStyle.h"
+#include "water_air_simpifield_formula.h"
+
+using namespace std;
+
+const double frmin = 20.;      //min frequency in Hz
+const double frmax = 20000.;  //max frequency in Hz
+
+int main()
+{
+  TCanvas *c = new TCanvas("c","c",1024,768);
+  gStyle->SetOptStat(0);
+  c->SetGrid(1,1);
+  TH1* frame = new TH1F("frame","frame",100,frmin,frmax);
+  frame->GetXaxis()->SetTitle("f [Hz]");
+  
+  double TCelsiusAir = 20;
+  double Humidity = 30;
+  double Pressure = 1;
+  frame->SetTitle(Form("#alpha versus f for T=%.0f C, H = %.0f%%, P = %.1f atm",TCelsiusAir,Humidity,Pressure));  
+  frame->GetYaxis()->SetTitle("#alpha [dB/100 m]");
+  frame->SetMinimum(0.001);
+  frame->SetMaximum(100);
+  frame->Draw();
+  TF1* air_absorption_orig = new TF1("air_absorption_orig",AFuncAir,frmin,frmax,NParsa);
+  air_absorption_orig->SetParameters(TCelsiusAir,Humidity,Pressure);
+  air_absorption_orig->SetLineColor(8);
+  air_absorption_orig->SetLineWidth(2);
+  air_absorption_orig->Draw("SAME");
+  TF1* air_absorption_func = new TF1("air_absorption_func",air_absorption,frmin,frmax,NParsa);
+  air_absorption_func->SetParameters(TCelsiusAir,Humidity,Pressure);
+  air_absorption_func->SetLineColor(2);
+  air_absorption_func->SetLineStyle(7);
+  air_absorption_func->Draw("SAME");
+  c->SetLogx(1);
+  c->SetLogy(1);
+  c->Modified();
+  c->Print(Form("air_approximation_T%.0f_H%.0f_P%.0f.png",TCelsiusAir,Humidity,Pressure));
+  
+  double TCelsiusWater = 8;
+  double Salinity = 35;
+  double Depth    = 50;
+  double pH       = 8;
+  frame->SetTitle(Form("#alpha versus f for T=%.0f C, S = %.0f ppt, D = %.0f m, pH = %.0f",TCelsiusWater,Salinity,Depth,pH));
+  frame->GetYaxis()->SetTitle("#alpha [dB/km]");
+  frame->SetMinimum(0.00001);
+  frame->SetMaximum(10);
+  frame->Draw();
+  TF1* water_absorption_orig = new TF1("water_absorption_orig",AFuncWater,frmin,frmax,NParsw);
+  water_absorption_orig->SetParameters(TCelsiusWater,Humidity,Pressure);
+  water_absorption_orig->SetLineColor(8);
+  water_absorption_orig->SetLineWidth(2);
+  water_absorption_orig->Draw("SAME");
+  TF1* water_absorption_func = new TF1("water_absorption_func",water_absorption,frmin,frmax,NParsw); 
+  water_absorption_func->SetParameters(TCelsiusWater,Salinity,Depth,pH);
+  water_absorption_func->SetLineColor(2);
+  water_absorption_func->SetLineStyle(7);
+  water_absorption_func->Draw("SAME");
+  c->SetLogx(1);
+  c->SetLogy(1);
+  c->Modified();
+  c->Print(Form("water_approximation_T%.0f_S%.0f_D%.0f_pH%.0f.png",TCelsiusWater,Salinity,Depth,pH));
+  
+  delete frame;
+  delete water_absorption_func;
+  delete water_absorption_orig;
+  delete air_absorption_func;
+  delete air_absorption_orig;
+  delete c;
+
+  return 0;
+}
